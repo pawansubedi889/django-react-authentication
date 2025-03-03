f63452413b4d1bd3e5919184eb3e1e3d99af9dae
Pawan Subedi <pawansubedi90558@gmail.com>
2025-03-03 11:08:24 +0545

    cors, drf and simplejwt added

--- server/core/settings.py
+++ server/core/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Modules
+    'corsheaders',
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
